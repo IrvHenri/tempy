0e4d089fed59218a26d9b7e2c9f79e4c51077001
Irving Henriquez <henriquezirv@gmail.com>
2021-08-22 01:15:57 -0400

    added build middleware to app.js

--- server/app.js
+++ server/app.js
@@ -11,8 +11,10 @@ app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "build")));
 app.use("/", indexRouter);
 app.use("/api/cities", citiesRouter);
-
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 module.exports = app;
