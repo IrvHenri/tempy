e1f5c93619bfdaf50ab1efdc9b7573e36cc23705
Irving Henriquez <henriquezirv@gmail.com>
2021-08-21 22:15:43 -0400

    restored public folder and express.static

--- server/app.js
+++ server/app.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
+app.use(express.static(path.join(__dirname, "public")));
 app.use("/", indexRouter);
 app.use("/api/cities", citiesRouter);
 
